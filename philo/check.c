9a4e96536a55baa4beac140f5bf634ae585612f5
Flavio D'agnese <fd-agnes@e3r4p5.42roma.it>
2021-07-24 11:27:55 +0200

    Checker fixato

--- philo/check.c
+++ philo/check.c
@@ -6,7 +6,7 @@
 /*   By: fd-agnes <fd-agnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/21 18:10:52 by fd-agnes          #+#    #+#             */
-/*   Updated: 2021/07/24 10:53:41 by fd-agnes         ###   ########.fr       */
+/*   Updated: 2021/07/24 11:25:25 by fd-agnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int	ft_single_arg_check(const char *str)
 	int	i;
 
 	i = 0;
+	if (str[i] == 0)
+		return (-1);
 	while (str[i] && str[i] != 0)
 	{
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
@@ -56,10 +58,12 @@ int	ft_check_args(int ac, char **av)
 void	ft_death_check(t_data *d, t_philo *p)
 {
 	int	i;
+	int	j;
 
 	while (!(d->all_ate))
 	{
 		i = 0;
+		j = 0;
 		while (i < d->nb_philo && !(d->is_dead))
 		{
 			pthread_mutex_lock(&(d->meal_check));
@@ -67,13 +71,14 @@ void	ft_death_check(t_data *d, t_philo *p)
 			{
 				ft_print(d, &p[i], DIE);
 				d->is_dead = 1;
+				while (j++ < d->nb_philo)
+				{
+					pthread_mutex_unlock(&(d->fork[p->left_fork_id]));
+					pthread_mutex_unlock(&(d->fork[p->right_fork_id]));
+				}
 			}
 			pthread_mutex_unlock(&(d->meal_check));
-//			usleep(100);
 			i++;
 		}
-		if (d->is_dead)
-			break ;
-		i = 0;
 	}
 }
